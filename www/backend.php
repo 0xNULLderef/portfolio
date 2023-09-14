39374629188b4c92e1022846a60e70e3e5f4669d
NULLderef <redmastersuper@gmail.com>
2023-09-14 10:10:09 +0200

    i hate frontend i hate frontend i hate frontend i hate frontend i hate frontend i hate frontend i hate frontend

--- www/backend.php
+++ www/backend.php
@@ -1,5 +1,7 @@
 <?php
 
+session_start();
+
 $connection = new mysqli(
 	"mariadb",
 	"root",
@@ -7,14 +9,14 @@ $connection = new mysqli(
 	"portfolio"
 );
 
-function fetch_text($name) {
+function fetch_single($table) {
 	global $connection;
-	return $connection->query("SELECT * FROM `text` WHERE `name`='" . $connection->real_escape_string($name) . "';")->fetch_array(MYSQLI_ASSOC);
+	return $connection->query("SELECT * FROM `" . $table . "`;")->fetch_array(MYSQLI_ASSOC);
 }
 
 function about() {
 	echo json_encode(
-		fetch_text("about")
+		fetch_single("about")
 	);
 }
 
@@ -25,24 +27,40 @@ function projects() {
 	);
 }
 
-function contact() {
-	echo json_encode(array());
+function skills() {
+	global $connection;
+	echo json_encode(
+		$connection->query("SELECT * FROM `skills`;")->fetch_all(MYSQLI_ASSOC)
+	);
 }
 
-function skills() {
-	echo json_encode(array());
+function login($data) {
+	global $connection;
+	$credentials = $connection->query("SELECT * FROM `credentials`;")->fetch_array(MYSQLI_ASSOC);
+	if(password_verify($data["password"], $credentials["hash"])) {
+		$_SESSION["authorized"] = true;
+		echo json_encode(array("success" => true));
+	} else {
+		echo json_encode(array("success" => false));
+	}
+	// $data["password"]
+}
+
+function status($session) {
+	echo json_encode(array("authorized" => $session["authorized"] ?? false));
 }
 
 function not_found() {
 	http_response_code(404);
-	echo json_encode(array("error" => "Page not found!"));
+	echo json_encode(array("error" => "Endpoint not found!"));
 }
 
 match(array($_SERVER["REQUEST_METHOD"], array_merge($_GET, $_POST)["endpoint"] ?? null)) {
 	array("GET", "about") => about(),
 	array("GET", "projects") => projects(),
-	array("GET", "contact") => contact(),
 	array("GET", "skills") => skills(),
+	array("POST", "login") => login($_POST),
+	array("GET", "status") => status($_SESSION),
 	default => not_found(),
 };
 
--- www/script.js
+++ www/script.js
@@ -5,57 +5,111 @@
  */
 
 // modify on page add
+
+const setInnerText = (element, value) => {
+	element.innerText = value;
+};
+
+const setSrc = (element, value) => {
+	element.src = value;
+};
+
+const set = {
+	H1: setInnerText,
+	P: setInnerText,
+	IMG: setSrc
+};
+
+const processGeneric = (data, section) => {
+	Object.entries(data).forEach(([property, value]) => {
+		const element = section.querySelector(`[property=${property}]`);
+		set[element.tagName](element, value);
+	});
+};
+
+const clearChildren = element => { while(element.firstChild) element.removeChild(element.lastChild); };
+
 const pages = [
 	{
 		name: 'about',
-		process: (data, section) => {
-			console.warn('Stub!');
-		}
+		process: processGeneric
 	},
 	{
 		name: 'projects',
 		process: (data, section) => {
-			console.warn('Stub!');
-		}
-	},
-	{
-		name: 'contact',
-		process: (data, section) => {
-			console.warn('Stub!');
+			clearChildren(section);
+			data.forEach(project => {
+				const row = document.createElement('div');
+				row.classList.add('project', 'row', 'my-2');
+				section.appendChild(row);
+				const col = document.createElement('div');
+				col.classList.add('col-12', 'p-5', 'd-flex', 'align-items-center', 'bg-body-secondary');
+				row.appendChild(col);
+				const container = document.createElement('div');
+				container.classList.add('container');
+				col.appendChild(container);
+				const a = document.createElement('a');
+				a.classList.add('portfolio-highlight', 'text-decoration-none');
+				a.href = project.url;
+				container.appendChild(a);
+				const h1 = document.createElement('h1');
+				h1.appendChild(document.createTextNode(project.name));
+				a.appendChild(h1);
+				const p = document.createElement('p');
+				p.appendChild(document.createTextNode(project.description));
+				container.appendChild(p);
+			});
 		}
 	},
 	{
 		name: 'skills',
 		process: (data, section) => {
-			console.warn('Stub!');
+			clearChildren(section);
+			data.forEach(skill => {
+				const row = document.createElement('div');
+				row.classList.add('skill', 'row', 'my-2');
+				section.appendChild(row);
+				const col = document.createElement('div');
+				col.classList.add('col-12', 'p-5', 'd-flex', 'bg-body-tertiary');
+				row.appendChild(col);
+				const container = document.createElement('div');
+				container.classList.add('container');
+				col.appendChild(container);
+				const h3 = document.createElement('h3');
+				h3.appendChild(document.createTextNode(skill.name));
+				container.appendChild(h3);
+				const progress = document.createElement('div');
+				progress.classList.add('progress');
+				progress.setAttribute('role', 'progressbar');
+				container.appendChild(progress);
+				const bar = document.createElement('div');
+				bar.classList.add('progress-bar', 'portfolio-highlight-background');
+				bar.style = `width: ${skill.percentage}%`;
+				bar.appendChild(document.createTextNode(`${skill.percentage}%`));
+				progress.appendChild(bar);
+			});
 		}
 	},
 ];
 
+const fetchBackend = async (endpoint, params) => fetch('backend.php?endpoint=' + endpoint, params ?? null).then(r => r.json());
 
-const fetchPageData = async page => fetch('backend.php?endpoint=' + page).then(r => r.json());
+const getPageSection = pageSection => document.querySelector('[section=' + pageSection + ']');
 
-const getPageSection = pageSection => document.querySelector('section[page=' + pageSection + ']');
+pages.forEach(async page => page.process(await fetchBackend(page.name), getPageSection(page.name)));
 
-/*const displayPage = async page => ({
-	about: (data, section) => {
-		//const container = document.create
-		const img = document.createElement('img');
-		img.src = data.image;
-		section.append(img);
-		const p = document.createElement('p');
-		p.innerText = data.text;
-		section.append(p);
+document.getElementById('login-form').addEventListener('submit', async function(event) {
+	event.preventDefault();
+
+	const response = await fetchBackend(
+		'login',
+		{
+			method: 'POST',
+			body: new FormData(this)
+		}
+	);
+	if(response.success) {
+
 	}
-})[page](await fetchPageData(page), getPageSection(page));*/
-
-window.addEventListener('load', async () => {
-	/*document.querySelectorAll('.nav-item').forEach(item => {
-		item.addEventListener('click', event => {
-			event.preventDefault();
-		});
-	});*/
-
-	pages.forEach(async page => page.process(await fetchPageData(page.name), getPageSection(page.name)));
 });
 
